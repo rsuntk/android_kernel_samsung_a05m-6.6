d3b968f7290c31323c35cafe9cc20bb055155cc1
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-05 22:33:31 +0700

    kernel: printk: Filter out some userspace logs
    Co-authored-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -792,6 +792,11 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 				facility = LOG_FACILITY(u);
 			endp++;
 			line = endp;
+
+			if (strstr(line, "healthd") ||
+			    strncmp(line, "logd: Skipping", sizeof("logd: Skipping"))) {
+				return ret;
+			}
 		}
 	}
 
