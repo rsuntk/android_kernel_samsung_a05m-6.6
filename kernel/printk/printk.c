650543247e0205ee5273e4ef44af7a02d1724684
ris <rissu.ntk@gmail.com>
2026-07-06 01:35:32 +0700

    Revert "kernel: printk: use buffer from the stack space"
    * Breaks compilation
    
    This reverts commit 6fe5f79c708eff245f1daa396c57e97d236bafa9.
    
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -740,7 +740,7 @@ int devkmsg_emit(int facility, int level, const char *fmt, ...)
 
 static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 {
-	char buf[PRINTKRB_RECORD_MAX + 1], *line;
+	char *buf, *line;
 	int level = default_message_loglevel;
 	int facility = 1;	/* LOG_USER */
 	struct file *file = iocb->ki_filp;
@@ -761,9 +761,15 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			return ret;
 	}
 
+	buf = kmalloc(len+1, GFP_KERNEL);
+	if (buf == NULL)
+		return -ENOMEM;
+
 	buf[len] = '\0';
-	if (!copy_from_iter_full(buf, len, from))
+	if (!copy_from_iter_full(buf, len, from)) {
+		kfree(buf);
 		return -EFAULT;
+	}
 
 	/*
 	 * Extract and skip the syslog prefix <[0-9]*>. Coming from userspace
@@ -800,6 +806,7 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	}
 
 	devkmsg_emit(facility, level, "%s", line);
+	kfree(buf);
 	return ret;
 }
 
@@ -1586,11 +1593,15 @@ static int syslog_print(char __user *buf, int size)
 {
 	struct printk_info info;
 	struct printk_record r;
-	char text[PRINTK_MESSAGE_MAX];
+	char *text;
 	int len = 0;
 	u64 seq;
 
-	prb_rec_init_rd(&r, &info, &text, PRINTK_MESSAGE_MAX);
+	text = kmalloc(PRINTK_MESSAGE_MAX, GFP_KERNEL);
+	if (!text)
+		return -ENOMEM;
+
+	prb_rec_init_rd(&r, &info, text, PRINTK_MESSAGE_MAX);
 
 	mutex_lock(&syslog_lock);
 
@@ -1678,6 +1689,7 @@ static int syslog_print(char __user *buf, int size)
 	} while (size);
 out:
 	mutex_unlock(&syslog_lock);
+	kfree(text);
 	return len;
 }
 
