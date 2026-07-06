063733679782e1cad989e9ff25e270ebb9015174
ris <rissu.ntk@gmail.com>
2026-07-06 10:55:59 +0700

    Revert "sched/core: Always panic when scheduling in atomic context"
    This reverts commit db06410851b04f6e6a6fdc15c4e6f7bc36c58e03.
    
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6081,7 +6081,12 @@ static noinline void __schedule_bug(struct task_struct *prev)
 		pr_err("Preemption disabled at:");
 		print_ip_sym(KERN_ERR, preempt_disable_ip);
 	}
-	panic("scheduling while atomic\n");
+	check_panic_on_warn("scheduling while atomic");
+
+	trace_android_rvh_schedule_bug(prev);
+
+	dump_stack();
+	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
 }
 
 /*
